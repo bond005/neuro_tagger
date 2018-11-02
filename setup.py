f967f444c40bdeeaaebcf4c3111a5a270f5dc2a7
Ivan Yu. Bondarenko <bond005@yandex.ru>
2018-11-02 16:46:13 +0700

    Bug in the RST long description has been fixed.

--- setup.py
+++ setup.py
@@ -2,40 +2,18 @@ from setuptools import setup, find_packages
 
 import neuro_tagger
 
-long_description = '''
-NeuroTagger
-============
 
-NeuroTagger is a text tagger based on recurrent neural network. It can
-be used as NER, dependency parser, morphoanalyzer etc.
+description = 'Text tagger, based on the ELMo embeddings and recurrent neural network, with the simple sklearn-like ' \
+              'interface'
 
-The goal of this project is creation of a simple Python package with
-the sklearn-like interface for solution of different tasks of text
-tagging (named entity recognition, dependency parsing, etc) in case
-number of labeled texts is very small (not greater than several
-thousands). Special word embeddings named as `ELMo<https://arxiv.org/abs/1802.05365>`_
-(Embeddings from Language Models) ensure this possibility, because these
-embeddings are contextual and they allow to design more simple and
-separable feature space for words in texts.
-
-ELMo embeddings are used as features of words in text, and different
-variants of neural network architecture (BiLSTM, hybrid BiLSTM-CRF or
-pure CRF) can be used as final classifier (tagger). I recommend to
-use a special `TensorFlow Hub ELMo<>https://tfhub.dev/google/elmo/2`_
-for English NLP tasks and a `DeepPavlov ELMo
-<http://docs.deeppavlov.ai/en/master/apiref/models/embedders.html#deeppavlov.models.embedders.elmo_embedder.ELMoEmbedder>`_
-for for same tasks in Russian.
-
-'''
 
 setup(
     name='neuro_tagger',
     version=neuro_tagger.__version__,
     packages=find_packages(exclude=['tests', 'demo']),
     include_package_data=True,
-    description='Text tagger, based on the ELMo embeddings and recurrent neural network, with the simple sklearn-like '
-                'interface',
-    long_description=long_description,
+    description=description,
+    long_description=description,
     url='https://github.com/bond005/neuro_tagger',
     author='Ivan Bondarenko',
     author_email='bond005@yandex.ru',
